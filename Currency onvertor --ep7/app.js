46c9274972aa5863b8ba010f5efc1ad712ed0198
shahidkhan98 <155524277+shahidkhan121@users.noreply.github.com>
2024-10-25 04:52:00 +0500

    Add files via upload

---
+++ Currency onvertor --ep7/app.js
@@ -0,0 +1,180 @@
+const fromAmountElement = document.querySelector(".amount");
+const convertedAmountElement = document.querySelector(".ConvertedAmount");
+const fromCurrencyElement = document.querySelector(".fromCurrency");
+const toCurrencyElement = document.querySelector(".toCurrency");
+const resultElement = document.querySelector(".result");
+resultElement.textContent = "fetching exchange rates .....";
+convertorContainer = document.querySelector(".convertor-container");
+
+const countries = [
+  { code: "AFN", name: "Afghan Afghani" },
+  { code: "USD", name: "United States Doller" },
+  { code: "ALL", name: "Albanian Lek" },
+  { code: "DZD", name: "Algerian Dinar" },
+  { code: "AOA", name: "Angolan Kwanza" },
+  { code: "ARS", name: "Argentine Peso" },
+  { code: "AUD", name: "Australian Dollar" },
+  { code: "AZN", name: "Azerbaijani Manat" },
+  { code: "BAM", name: "Bosnia and Herzegovina Convertible Mark" },
+  { code: "BDT", name: "Bangladeshi Taka" },
+  { code: "BGN", name: "Bulgarian Lev" },
+  { code: "BIF", name: "Burundian Franc" },
+  { code: "BRL", name: "Brazilian Real" },
+  { code: "CAD", name: "Canadian Dollar" },
+  { code: "CDF", name: "Congolese Franc" },
+  { code: "CHF", name: "Swiss Franc" },
+  { code: "CLP", name: "Chilean Peso" },
+  { code: "CNY", name: "Chinese Yuan" },
+  { code: "COP", name: "Colombian Peso" },
+  { code: "CRC", name: "Costa Rican Colón" },
+  { code: "CVE", name: "Cape Verdean Escudo" },
+  { code: "CZK", name: "Czech Koruna" },
+  { code: "DKK", name: "Danish Krone" },
+  { code: "DOP", name: "Dominican Peso" },
+  { code: "EGP", name: "Egyptian Pound" },
+  { code: "ERN", name: "Eritrean Nakfa" },
+  { code: "ETB", name: "Ethiopian Birr" },
+  { code: "EUR", name: "Euro" },
+  { code: "FJD", name: "Fijian Dollar" },
+  { code: "FKP", name: "Falkland Islands Pound" },
+  { code: "GBP", name: "British Pound" },
+  { code: "GEL", name: "Georgian Lari" },
+  { code: "GHS", name: "Ghanaian Cedi" },
+  { code: "GMD", name: "Gambian Dalasi" },
+  { code: "GNF", name: "Guinean Franc" },
+  { code: "GTQ", name: "Guatemalan Quetzal" },
+  { code: "GYD", name: "Guyanese Dollar" },
+  { code: "HKD", name: "Hong Kong Dollar" },
+  { code: "HNL", name: "Honduran Lempira" },
+  { code: "HRK", name: "Croatian Kuna" },
+  { code: "HTG", name: "Haitian Gourde" },
+  { code: "HUF", name: "Hungarian Forint" },
+  { code: "IDR", name: "Indonesian Rupiah" },
+  { code: "ILS", name: "Israeli New Shekel" },
+  { code: "INR", name: "Indian Rupee" },
+  { code: "IQD", name: "Iraqi Dinar" },
+  { code: "IRR", name: "Iranian Rial" },
+  { code: "ISK", name: "Icelandic Króna" },
+  { code: "JMD", name: "Jamaican Dollar" },
+  { code: "JOD", name: "Jordanian Dinar" },
+  { code: "JPY", name: "Japanese Yen" },
+  { code: "KES", name: "Kenyan Shilling" },
+  { code: "KGS", name: "Kyrgyzstani Som" },
+  { code: "KHR", name: "Cambodian Riel" },
+  { code: "KPW", name: "North Korean Won" },
+  { code: "KRW", name: "South Korean Won" },
+  { code: "KZT", name: "Kazakhstani Tenge" },
+  { code: "LAK", name: "Lao Kip" },
+  { code: "LBP", name: "Lebanese Pound" },
+  { code: "LKR", name: "Sri Lankan Rupee" },
+  { code: "LRD", name: "Liberian Dollar" },
+  { code: "LSL", name: "Lesotho Loti" },
+  { code: "LYD", name: "Libyan Dinar" },
+  { code: "MAD", name: "Moroccan Dirham" },
+  { code: "MDL", name: "Moldovan Leu" },
+  { code: "MGA", name: "Malagasy Ariary" },
+  { code: "MKD", name: "Macedonian Denar" },
+  { code: "MMK", name: "Myanmar Kyat" },
+  { code: "MNT", name: "Mongolian Tögrög" },
+  { code: "MRU", name: "Mauritanian Ouguiya" },
+  { code: "MUR", name: "Mauritian Rupee" },
+  { code: "MVR", name: "Maldivian Rufiyaa" },
+  { code: "MWK", name: "Malawian Kwacha" },
+  { code: "MXN", name: "Mexican Peso" },
+  { code: "MYR", name: "Malaysian Ringgit" },
+  { code: "MZN", name: "Mozambican Metical" },
+  { code: "NAD", name: "Namibian Dollar" },
+  { code: "NGN", name: "Nigerian Naira" },
+  { code: "NIO", name: "Nicaraguan Córdoba" },
+  { code: "NOK", name: "Norwegian Krone" },
+  { code: "NPR", name: "Nepalese Rupee" },
+  { code: "NZD", name: "New Zealand Dollar" },
+  { code: "OMR", name: "Omani Rial" },
+  { code: "PAB", name: "Panamanian Balboa" },
+  { code: "PEN", name: "Peruvian Sol" },
+  { code: "PGK", name: "Papua New Guinean Kina" },
+  { code: "PHP", name: "Philippine Peso" },
+  { code: "PKR", name: "Pakistani Rupee" },
+  { code: "PLN", name: "Polish Zloty" },
+  { code: "PYG", name: "Paraguayan Guaraní" },
+  { code: "QAR", name: "Qatari Rial" },
+  { code: "RON", name: "Romanian Leu" },
+  { code: "RSD", name: "Serbian Dinar" },
+  { code: "RUB", name: "Russian Ruble" },
+  { code: "RWF", name: "Rwandan Franc" },
+  { code: "SAR", name: "Saudi Riyal" },
+  { code: "SBD", name: "Solomon Islands Dollar" },
+  { code: "SCR", name: "Seychellois Rupee" },
+  { code: "SDG", name: "Sudanese Pound" },
+  { code: "SEK", name: "Swedish Krona" },
+  { code: "SGD", name: "Singapore Dollar" },
+  { code: "SHP", name: "Saint Helena Pound" },
+  { code: "SLL", name: "Sierra Leonean Leone" },
+  { code: "SOS", name: "Somali Shilling" },
+  { code: "SRD", name: "Surinamese Dollar" },
+  { code: "SSP", name: "South Sudanese Pound" },
+  { code: "STD", name: "São Tomé and Príncipe Dobra" },
+  { code: "SYP", name: "Syrian Pound" },
+  { code: "SZL", name: "Swazi Lilangeni" },
+  { code: "THB", name: "Thai Baht" },
+  { code: "TJS", name: "Tajikistani Somoni" },
+  { code: "TMT", name: "Turkmenistani Manat" },
+  { code: "TND", name: "Tunisian Dinar" },
+  { code: "TOP", name: "Tongan Paʻanga" },
+  { code: "TRY", name: "Turkish Lira" },
+  { code: "TTD", name: "Trinidad and Tobago Dollar" },
+  { code: "TWD", name: "New Taiwan Dollar" },
+  { code: "TZS", name: "Tanzanian Shilling" },
+  { code: "UAH", name: "Ukrainian Hryvnia" },
+  { code: "UGX", name: "Ugandan Shilling" },
+];
+
+countries.forEach((country) => {
+  const option1 = document.createElement("option");
+  const option2 = document.createElement("option");
+  option1.value = option2.value = country.code;
+  option1.textContent =
+    option2.textContent = `${country.code} (${country.name})`;
+  fromCurrencyElement.appendChild(option1);
+  toCurrencyElement.appendChild(option2);
+
+  // default values of select tags
+  fromCurrencyElement.value = "PKR";
+  toCurrencyElement.value = "USD";
+});
+
+// function to get exchange rate using APi
+const getExchangeRate = async () => {
+  const amount = parseFloat(fromAmountElement.value);
+  const fromCurrency = fromCurrencyElement.value;
+  const toCurrency = toCurrencyElement.value;
+
+  // fetch Data from APi
+
+  try {
+    const response = await fetch(
+      `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`
+    );
+    const data = await response.json();
+    // console.log(data)
+
+    const conversionRate = data.rates[toCurrency];
+    const ConvertedAmount = (amount * conversionRate).toFixed(3);
+
+    if (typeof conversionRate === "undefined") {
+      resultElement.textContent =
+        "exchange rate data is not available for selected countries";
+    } else {
+      convertedAmountElement.value = ConvertedAmount;
+      resultElement.textContent = `${amount} ${fromCurrency} = ${ConvertedAmount} ${toCurrency} `;
+    }
+  } catch (error) {
+    convertorContainer.innerHTML = `<h2>Error While Fetching Exchnage Rates</h2>`;
+  }
+};
+
+// fetching
+fromAmountElement.addEventListener("input", getExchangeRate);
+fromCurrencyElement.addEventListener("change", getExchangeRate);
+toCurrencyElement.addEventListener("change", getExchangeRate);
+window.addEventListener("load", getExchangeRate);
